dff162201738aa7370a303e3d3a7f8bbdad44e3b
Andrea <Andrea.Lunghi3@gmail.com>
2022-03-01 21:48:28 +0100

    Add event creation

--- commands/event.js
+++ commands/event.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { allowedEventRoles } = require("../config.json");
+const templates = require("./templates.json");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -34,12 +35,12 @@ module.exports = {
 
         .addStringOption((option) =>
           option
-          .setName("descrizione")
-          .setDescription("La descrizione dell'evento")
-          .setRequired(false)
+            .setName("descrizione")
+            .setDescription("La descrizione dell'evento")
+            .setRequired(false)
         )
     )
-    
+
     .addSubcommand((subcommand) =>
       subcommand
         .setName("template")
@@ -50,6 +51,7 @@ module.exports = {
             .setName("nome")
             .setDescription("Il nome dell'evento")
             .setRequired(true)
+            .addChoice("Amung Us", "amung_us")
         )
 
         .addStringOption((option) =>
@@ -62,15 +64,62 @@ module.exports = {
 
   async execute(interaction) {
     if (isVerifiedUser(interaction.message.member)) {
+      var event;
       if (interaction.subcommand.name === "nuovo") {
-        newEvent(interaction);
+        event = newEvent(interaction);
+      } else if (interaction.subcommand.name === "template") {
+        event = newEventFromTemplate(interaction);
       }
+
+      interaction.message.guild.scheduledEvents.create(event);
+
+      interaction.message.reply("Evento creato");
     }
   },
 };
 
+function newEvent(interaction) {
+  const { nome, canale, descrizione } = interaction.options;
+  const data = getDateFromString(interaction.options.data);
+  const guild = interaction.message.guild;
+
+  if (!data) {
+    interaction.message.reply("La data non è valida");
+    return;
+  }
+
+  if (!checkChannel(guild, canale)) {
+    interaction.message.reply("Il canale non è valido");
+    return;
+  }
+
+  return {
+    name: nome,
+    scheduledStartTime: data,
+    channel: canale,
+    description: descrizione,
+  };
+}
+
+function newEventFromTemplate(interaction) {
+  const { nome } = interaction.options;
+  const data = getDateFromString(interaction.options.data);
+
+  if (!data) {
+    interaction.message.reply("La data non è valida");
+    return;
+  }
+
+  return {
+    name: nome,
+    scheduledStartTime: data,
+    channel: templates.canale,
+    description: templates.descrizione,
+  };
+}
+
 function isVerifiedUser(user) {
-  allowedEventRoles.forEach(role => {
+  allowedEventRoles.forEach((role) => {
     if (user.roles.cache.has(role)) {
       return true;
     }
@@ -79,8 +128,14 @@ function isVerifiedUser(user) {
   return false;
 }
 
-function newEvent (interaction) {
+function getDateFromString(string) {
+  const strings = string.split(" ");
+
+  const dateTime = strings[0] + "T" + strings[1];
+
+  return Date.parse(dateTime);
 }
 
-function newEventFromTemplate (interaction) {
+function checkChannel(guild, channelName) {
+  return guild.channels.cache.has(channelName);
 }
